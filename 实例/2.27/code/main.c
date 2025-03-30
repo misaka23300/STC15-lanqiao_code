db7633b1aee7e99054877565b77cc0445c958cae
laffey <laffey4590@gmail.com>
2025-03-30 19:58:03 +0800

    add

--- 实例/2.27/code/main.c
+++ 实例/2.27/code/main.c
@@ -22,21 +22,29 @@ enum {
 
 //bit sonic_flag, state_flag, key_flag, temp_flag, dac_flag;
 
-
-
 // 超声波
-uchar distance;
-bit sonic_flag;
-bit sonic_unit;
+struct 
+{
+    uchar distance;
+    bit sonic_flag;
+    bit sonic_unit;
+} sonic;
 
 // 键盘
-//uchar press;
-bit key_flag;
-uchar state;
+struct 
+{
+    bit key_flag;
+    uchar state;
+};
 
-// DS18B20
-int temperature;
-bit ds18b20_flag;
+
+struct
+{
+    int value;
+    bit ds18b20_flag;
+} temp;
+
+
 
 // LED
 extern uchar led_value[8];
@@ -47,20 +55,24 @@ bit led_flag;
 // 数码管
 extern uchar seg[8];
 bit state_flag;
-uchar state_1_flag;
+uchar state_1;
 uchar state_2;
 
 // DAC
 uchar DAC_value;
 bit DAC_flag;
 
-uchar low_dac;
+//0.1 - 2.0 * 10
+// 1 - 20
+uchar  low_dac;
 
 // other
 uchar distance_argument;
 uchar temp_argument;
 
 char calibra_value;
+
+uchar speed;
 
 void main()
 {
@@ -148,6 +160,8 @@ void state_proc()
     {
         case 0:
         {
+
+            // 2 3. 5 - x x 2 5
             set_seg(
                 temperature / 1000 % 10,
                 (temperature / 100 % 10) + 32,
@@ -163,10 +177,11 @@ void state_proc()
 
         case 1:
         {
-            switch (state_1_flag)
+            switch (state_1)
             {
                 case 0:
                 {
+                    // P 1 x x x x 4 0
                     set_seg(
                         24,
                         1,
@@ -179,6 +194,7 @@ void state_proc()
 
                 case 1:
                 {
+                    // P 2 x x x x 3 0
                     set_seg(
                         24,
                         2,
@@ -204,19 +220,22 @@ void state_proc()
                     calibra_list[2] = calibra_value % 10;
                     
                     delete_0(calibra_list, 3, calibra_value < 0 ? 1 : 0);
-
+                    
+                    // F 1 x x x 0 x x 5
                     set_seg( 15, 1, 16, 16, 16, calibra_list[0], calibra_list[1], calibra_list[2] );
                 }
                 break;
 
                 case 1:
                 {
-                    set_seg(15, 2, 16, 16, 16, 3, 4, 0);
+                    // F 2 x x x 3 4 0
+                    set_seg(15, 2, 16, 16, 16, speed / 100 % 10, speed / 10 % 10, speed % 10);
                 }
                 break;
 
                 case 2:
                 {
+                    // F 3 x x x x 0. 2
                     set_seg(15, 3, 16, 16, 16, 16,low_dac / 10 % 10, low_dac % 10);
                 }
                 break;
@@ -272,23 +291,72 @@ void key_proc()
 
         case 5:
         {
-            if (sonic_unit == 1)
-                sonic_unit = 0;
-            else
-                sonic_unit = 1;
+            switch (state)
+            {
+                case 0:
+                {
+                    if (sonic_unit == 1)
+                    sonic_unit = 0;
+                    else
+                        sonic_unit = 1;
+                }
+                break;
+
+                case 1:
+                {
+                    state_2 = (state_2 + 1) % 2;
+                }
+                break; 
+            }
+                       
 					
         }
         break;
 
         case 8:
         {
-
+            if (state == 2)
+            {
+                if (state_2 == 0 && distance_argument != 90)
+                {
+                    distance_argument = distance_argument + 10;
+                }
+                else if (state_2 == 1 && speed != 90)
+                {
+                    speed = speed + 10;
+                }
+                else if (state_2 == 2 && low_dac != 20)
+                {
+                    low_dac = low_dac + 1;
+                }
+            }
+            else if (state == 1)
+            {
+                //dac_proc();
+            }
         }
         break;
 
         case 9:
         {
+            if (state == 2)
+            {
+                switch (state_2)
+                {
+                    case 0:
+                    {
+                        if (distance_argument != 10)
+                            distance_argument = distance_argument - 10;
+                    }
+                    break;
 
+                    case 1:
+                    {
+                        //if ()
+                    }
+                    break;
+                }
+            }
         }
         break;
     }
