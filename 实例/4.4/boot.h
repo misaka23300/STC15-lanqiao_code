1e115031beed2c4d99967707065bca3f697149c8
laffey <laffey4590@gmail.com>
2025-04-05 14:10:13 +0800

    14-

---
+++ 实例/4.4/boot.h
@@ -0,0 +1,19 @@
+/*
+#ifndef __ _H__
+#define __ _H__
+
+#include "boot.h"
+
+
+#endif
+*/
+
+#ifndef __BOOT_H__
+#define __BOOT_H__
+
+#define uchar unsigned char
+#define uint nnsigned int
+
+#define ichar idata unsigned char 
+
+#endif
