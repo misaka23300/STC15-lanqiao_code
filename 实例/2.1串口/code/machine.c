b8bf51a5ea347fdd88cee6b855bb12a0aacf4cb2
misaka23300 <laffey4590@gmail.com>
2025-06-05 20:46:24 +0800

    feature: 新增了串口

---
+++ 实例/2.1串口/code/machine.c
@@ -0,0 +1,15 @@
+#include "machine.h"
+
+
+void batch(uchar i)
+{
+    switch (i)
+    {
+        case 4: { P2 = (P2 & 0x1F) | 0x80; break; }
+        case 5: { P2 = (P2 & 0x1F) | 0xA0; break; }
+        case 6: { P2 = (P2 & 0x1F) | 0xC0; break; }
+        case 7: { P2 = (P2 & 0x1F) | 0xE0; break; }
+    }
+
+    P2 = P2 & 0x1F;
+}
