111ab0550dfd9df364a701a7ef82b79c8bf2cad2
laffey <84133272+misaka23300@users.noreply.github.com>
2026-02-02 19:07:49 +0800

    fix: 修复变量初始化的错误

--- 模块/1.4 sonic/main/main.c
+++ 模块/1.4 sonic/main/main.c
@@ -12,21 +12,16 @@ sonic_t sonic;
 key_t key;
 
 
-void sonic_display()
-{
-    set_seg(0, sonic.distance / 100 % 10);
-    set_seg(1, sonic.distance / 10 % 10);
-    set_seg(2, sonic.distance % 10);
-}
+
 
 void key_task()
 {
     if (key.press == 4) {
-        if (sonic.mode == 0) {
-            sonic.mode = 1;
+        if (sonic.mode == 1) {
+            sonic.mode = 2;
         }
-        else if (sonic.mode == 1) {
-            sonic.mode = 0;
+        else if (sonic.mode == 2) {
+            sonic.mode = 1;
         }
     }
 }
@@ -41,26 +36,17 @@ void sonic_task()
         sonic.distance = sonic_measure_mode2();
     }
 
-    sonic_display();
+    set_seg(0, sonic.distance / 100 % 10);
+    set_seg(1, sonic.distance / 10 % 10);
+    set_seg(2, sonic.distance % 10);
 }
 void main()
 {
     boot_init();
-
+    sonic.mode = 1;
     while (1)
     {
-        /* if (sonic_flag)
-        {
-            sonic_flag = 0;
-            distance = sonic_measure();
-        }
-
-        if (state_flag)
-        {
-            state_flag = 0;
-            state_proc();
-        } */
-
+       
         if (sonic.time == SONIC_TIME) {
             sonic.time = 0;
             sonic_task();
